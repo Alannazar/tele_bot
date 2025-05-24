180373041a2eef11f5178cc0c55419e471dd8872
Alannazar <66084003+Alannazar@users.noreply.github.com>
2025-05-24 18:21:58 +0500

    Удалены
    Удалены секретные файлы из репозитория

--- bot.py
+++ bot.py
@@ -5,17 +5,17 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
-from handlers import language, menu, booking, menu_categories
 from dotenv import load_dotenv
+
 from handlers import language, menu, booking, menu_categories
 from handlers import cart
-# from handlers import first_course_view
 from handlers import dish_view
 from handlers import payment
 from handlers.delivery_menu import router as delivery_router
 from handlers import reviews
 
-load_dotenv()
+# Загружаем .env из другого пути
+load_dotenv(dotenv_path="C:/Users/ALAN/Desktop/key/.env")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
@@ -28,13 +28,11 @@ dp.include_routers(
     menu.router,
     booking.router,
     menu_categories.router,
-    # first_course_handler.router,
     cart.router,
-    # first_course_view.router,
     dish_view.router,
     payment.router,
     delivery_router,
-    reviews.router  # добавлено
+    reviews.router
 )
 
 # Запуск
