bbc840ac939138f87e73ea0ba61a98299cb68713
Alannazar <66084003+Alannazar@users.noreply.github.com>
2025-05-24 22:02:04 +0500

    webhook

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
-import asyncio
 import logging
 import os
+
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 from dotenv import load_dotenv
 
+# Роутеры
 from handlers import language, menu, booking, menu_categories
 from handlers import cart
 from handlers import dish_view
@@ -14,15 +17,18 @@ from handlers import payment
 from handlers.delivery_menu import router as delivery_router
 from handlers import reviews
 
-# Загружаем переменные из .env (если локально)
+# Загрузка переменных среды
 load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-storage = MemoryStorage()
-dp = Dispatcher(storage=storage)
+# Настройки Webhook
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_HOST = f"https://{os.getenv('RENDER_EXTERNAL_HOSTNAME')}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-# Подключаем все роутеры
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
 dp.include_routers(
     language.router,
     menu.router,
@@ -35,10 +41,22 @@ dp.include_routers(
     reviews.router
 )
 
-# Запуск
-async def main():
-    logging.basicConfig(level=logging.INFO)
-    await dp.start_polling(bot)
+async def on_startup(app):
+    logging.warning("Запуск бота...")
+    await bot.set_webhook(WEBHOOK_URL)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def on_shutdown(app):
+    logging.warning("Остановка бота...")
+    await bot.delete_webhook()
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+    app = web.Application()
+    app["bot"] = bot
+
+    SimpleRequestHandler(dispatcher=dp, bot=bot).register(app, path=WEBHOOK_PATH)
+    app.on_startup.append(on_startup)
+    app.on_shutdown.append(on_shutdown)
+
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
